3b3e70e6932f8da056bfe51eb962e283dae7911a
Colin Park <colinpark100@gmail.com>
2025-07-24 15:35:55 -0400

    did some work with the js file and added functionality to html

---
+++ app.js
@@ -0,0 +1,26 @@
+const output = document.querySelector(".output-box > h1");
+const buttons = document.querySelector(".btn");
+
+const array = [];
+
+function addToArray(input) {
+
+    checkInput(input);
+    array.push(input);
+
+}
+
+function checkInput(input) {
+    if (input === )
+}
+
+// figure out algorithm
+function printToDisplay() {
+    for (let i = 0; i < array.length; i++) {
+        
+    }
+}
+
+function clearArray() {
+    array = [];
+}
