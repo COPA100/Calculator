52af8de36f33304545a0ce851b0148032cd5f899
COPA100 <colinpark100@gmail.com>
2025-07-25 18:55:36 -0400

    worked on js code and added functionality for buttons

--- app.js
+++ app.js
@@ -1,26 +1,74 @@
 const output = document.querySelector(".output-box > h1");
-const buttons = document.querySelector(".btn");
 
-const array = [];
+let array = [];
 
+//values added to array and when equals is pressed it compiles
 function addToArray(input) {
-
-    checkInput(input);
     array.push(input);
-
+    checkInput();
+    printToDisplay();
 }
 
-function checkInput(input) {
-    if (input === )
+
+function checkInput() {
+    if (isOperator(array[0])) {
+        clearArray();
+    }
+    else if (array.length >= 2 && isOperator(array[array.length - 1]) && isOperator(array[array.length - 2])) {
+        array.splice(array.length - 2, 1);
+    }
+    else if (array.length - 1 === "=") {
+        compileArray();
+    }
 }
 
-// figure out algorithm
-function printToDisplay() {
+function compileArray() {
+    const merged = [];
     for (let i = 0; i < array.length; i++) {
         
     }
 }
 
+function isOperator(input) {
+    switch (input){
+        case "%":
+            return true;
+            break;
+        case "÷":
+            return true;
+            break;
+        case "–":
+            return true;
+            break;
+        case "+":
+            return true;
+            break;
+        case "×":
+            return true;
+            break;
+        default:
+            return false;
+    }
+}
+
+function printToDisplay() {
+    let string = "";
+    for (let i = 0; i < array.length; i++) {
+        string += array[i];        
+    }
+    output.textContent = string;
+}
+
 function clearArray() {
     array = [];
+    output.textContent = "0";
+    console.clear();
+}
+
+//for debugging
+function dispArrayConsole() {
+    for(let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+    console.log("\n");
 }
